77b43a1a89d22d3a192d97c7f50235cc63592048
Raj <rajguptasamrat520@gmail.com>
2022-12-15 18:17:46 +0530

    i have done ejs

---
+++ controllers/post_controller.js
@@ -0,0 +1,3 @@
+module.exports.post = function(req,res){
+    res.end('<h1>This is first my post to this social app!</h1>')
+}
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,10 @@ console.log('router loadded');
 
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'))
+// router.use('/users/post',require('./post'));
+
+//for any further routes, access from here
+//router .use('/routerName', require('./routerfile))
 
 module.exports = router;

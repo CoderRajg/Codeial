54f44704a29cc7c1f1dbd7fff27878253dee35ab
Raj <rajguptasamrat520@gmail.com>
2022-12-19 18:40:38 +0530

    i ahve done till now

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose  = require("mongoose");
+
+// mongoose.connect("mongodb://localhost/codeial_development");
+mongoose.connect('mongodb://localhost/contacts_list_db', { useUnifiedTopology: true, family: 4, useNewUrlParser: true }).
+  then(() => console.log('Successfully Connected to Database')).
+  catch(err => console.log('Caught', err.stack));
+
+const db = mongoose.connection;
+
+module.exports = db;
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,27 @@
 module.exports.profile = function(req,res){
     res.end('<h1>Users profile</h1>')
 }
+
+//render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "codeial | sign up"
+    })
+}
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "codeial | Sign In"
+    })
+}
+
+//get the sign up data
+module.exports.create = function(req,res){
+    //to do later
+}
+
+//sign in and create the session for the user
+module.exports.createSession = function(req,res){
+    //to do later
+}
